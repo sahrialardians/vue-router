1f7650cfbdc62f71b3429a7d7b8f18a068111543
sahrialardians <sahrialardians@gmail.com>
2020-03-17 19:31:58 +0700

    modified style

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="profile">
       <div class="container">
-          <div class="row">
-              <div class="col-12">
+          <div class="row justify-content-center">
+              <div class="col-lg-6">
                   <div class="media text-left">
-                    <img src="../assets/photo-profile.png" class="mr-3 img-thumbnail" alt="">
+                    <img src="../assets/photo-profile.png" class="mr-3 img-profile" alt="">
                     <div class="media-body">
                         <h5 class="mt-0">Sahrial Ardians</h5>
                         <p>
-                            I am a web developer based in Medan, Indonesia. And I focus on back-end developers. But I also have a passion 
-                            <br>for website design and mobile devices.
+                            I am a web developer based in Medan, Indonesia. And I focus on back-end developers. But I also have a passion for website design and mobile devices.
                         </p>
                     </div>
                   </div>
@@ -31,3 +30,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+    .img-profile{
+        width: 100px;
+        height: 100px;
+        border-radius: 200%;
+    }
+</style>
