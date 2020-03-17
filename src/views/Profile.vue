c5f24653569252744b5e6255f5914881d1f91ea3
sahrialardians <sahrialardians@gmail.com>
2020-03-17 17:35:03 +0700

    create new file Profile.vue

---
+++ src/views/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="profile">
+    <h1>This is an profile page</h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Profile',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
