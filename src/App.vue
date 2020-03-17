995dd6496e70c12a994ebab13330e43319b263bc
sahrialardians <sahrialardians@gmail.com>
2020-03-17 17:37:01 +0700

    solve navigation error

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">Profile</router-link> |
+      <router-link to="/profile">Profile</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
